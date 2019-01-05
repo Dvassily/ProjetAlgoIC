67fd5aef7dbbc4063f645c8d2addabc4df7ba6d9
Simon Serrano <simon.serrano@etu.unice.fr>
2019-01-05 13:47:50 +0100

    Fixed random generator for the second time

--- uniform.py
+++ uniform.py
@@ -1,5 +1,5 @@
 import networkx as nx
-from random import choice, random
+from random import random
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -7,32 +7,21 @@ def uniforme(a,b):
     return int(a+(b-a)*random())
 
 
-def build_subcomplex(size, graph):
-    subcomplex = list()
-    random_node = np.random.choice(graph.nodes())
-    subcomplex.append(random_node)
-    while len(subcomplex)<size:
-        random_node = np.random.choice(list(graph.neighbors(random_node)))
-        if random_node not in subcomplex : subcomplex.append(random_node) 
-    return subcomplex
-
-def build_full_graph(n):
-    return nx.complete_graph(n)
-
 def instances(n=1, t=1):
-    #Build a graph of size n
-    graph = build_full_graph(n)
-    subcomplexes = list()
+    complexes = list()
+    size = uniforme(1, n)
     for i in range(t):
-        subcomplexes.append(build_subcomplex(uniforme(1, n), graph))
-    
-    return graph, subcomplexes
+        subcomplex = list()
+        j=0
+        while j <size:
+            node = np.random.choice(range(n))
+            if node not in subcomplex :
+                subcomplex.append(node)
+                j+=1
+        complexes.append(subcomplex)
+    return complexes
 
 
 
-graph, subcomplexes = instances(100, 2)
-nx.draw(graph)
-plt.show()
-print(subcomplexes)
-    
-
+complexes = instances(100, 6)
+print(complexes)
